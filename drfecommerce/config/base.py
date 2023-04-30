6fbb49744d7e8be4b0e698e090d84b497f23a2c7
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 01:21:42 +0000

    providing an option for working production

--- drfecommerce/config/base.py
+++ drfecommerce/config/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@40n1o1!q%sg38_cn+2^%9k)^mh+us0jgfrlm642986vlgvuz+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from drfecommerce.config import base
+
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.config.local')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.config.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.config.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
