12122bbd6c137495e6001b55382d46e865919ee2
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 01:57:11 +0000

    .evn file added to hide value content

--- drfecommerce/config/base.py
+++ drfecommerce/config/base.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@40n1o1!q%sg38_cn+2^%9k)^mh+us0jgfrlm642986vlgvuz+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+SECRET_KEY=os.environ.get("SECRET_KEY")
 
 
 # Application definition
